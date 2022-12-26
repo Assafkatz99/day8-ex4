c69e766a227a9872dbbee74c48ad70c4bee8f681
assafkatz99 <assafkatz99@gmail.com>
2022-12-26 16:47:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+numbers1 = [1,2,3,4,5]
+numbers2 = numbers1
+numbers1.push(6)
+console.log(numbers1);
+console.log(numbers2);
+
+
+
+numbers3 = [1,2,3,4,5]
+numbers4 = numbers3.slice()
+numbers3.push(6)
+console.log(numbers3);
+console.log(numbers4);
+
